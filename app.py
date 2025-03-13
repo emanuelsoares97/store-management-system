21c102c4f12a7eabce8eeed49e0bc298c8dbfbf0
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-13 17:38:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes import init_routes
-from config import DEBUG
+from config import DEBUG,SECRET_KEY
 
 app = Flask(__name__)
+app.config["SECRET_KEY"]= SECRET_KEY
 app.config["DEBUG"] = DEBUG
+
 
 # Inicializa as rotas
 init_routes(app)
