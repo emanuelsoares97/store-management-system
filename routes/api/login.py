5634ecad0b05983c01017a9e06a7def61949a29b
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-15 15:42:18 +0000

    Update login.py

--- routes/api/login.py
+++ routes/api/login.py
@@ -1,52 +1,22 @@
-from flask import jsonify, request, render_template, make_response, session, Blueprint
+from flask import Blueprint, request, jsonify, make_response
+from services.utilizadoresmanager import UtilizadorService
+from util.auth import AuthService 
 
-from data.json_manager import carregar_lista
-from config import DEBUG, SECRET_KEY
-from util.auth import token_required
-from util.logger_util import get_logger
-import jwt
-from datetime import datetime, timedelta
+login_bp = Blueprint("login", __name__)
 
-
-login_bp=Blueprint("login", __name__)
-
-routelog = get_logger("login_routes")
-
-
-
-
-"""
-    ROTA LOGIN:
-    - /login (POST) - Envia dados para o servidor e é feita a validação   - /auth (GET)   - Recebe os dados enviados pelo cliente caso seja autorizado faz o login
-
-"""
-
-
-@login_bp.route("/login", methods=["POST"])
+@login_bp.route("/", methods=["POST"])
 def login():
-    """Autenticação de utilizador """
+    """Autenticação de utilizador"""
     data = request.get_json()
+    token = UtilizadorService.autenticar(data.get("email"), data.get("password"))
 
-    username = data.get("username")
-    password = data.get("password")
-
-    # Simulação de verificação de credenciais
-    if username == "admin" and password == "123456":
-        session["logged_in"] = True
-        token = jwt.encode(
-            {
-                "user": username,
-                "exp": datetime.utcnow() + timedelta(seconds=120)  # Token expira em 2 minutos
-            }, 
-            SECRET_KEY,
-            algorithm="HS256"
-        )
+    if token:
         return jsonify({"token": token})
-    else:
-        return make_response(jsonify({"erro": "Credenciais inválidas"}), 403)
+
+    return make_response(jsonify({"erro": "Credenciais inválidas"}), 403)
 
 @login_bp.route("/auth", methods=["GET"])
-@token_required
+@AuthService.token_required
 def auth():
-    """retorna 'sucesso' se for valido"""
-    return jsonify({"mensagem": "JWT verificado, bem-vindo!"})
+    """Rota protegida que verifica se o token é válido"""
+    return jsonify({"mensagem": "Token válido, utilizador autenticado!"})
