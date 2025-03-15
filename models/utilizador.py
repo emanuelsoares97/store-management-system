962309b596ffba2691d0e4b1b55336a6c81fdaef
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-15 15:43:22 +0000

    Create utilizador.py

---
+++ models/utilizador.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from models.abstrata import BaseModel
+
+class Utilizador(BaseModel.get_base()):  
+    __tablename__ = "utilizadores"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)  #Garantir que `id` fica primeiro!
+    nome = Column(String(100), nullable=False)
+    email= Column(String, nullable=False)
+    password=Column(String, nullable=False)
+    role = Column(String, default="user") 
