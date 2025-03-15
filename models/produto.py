91e464ed1a3eeb7386212bd5bd5958f136d85c73
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-15 10:11:30 +0000

    alterada toda a estrutura da classe produto uma vez que é uma subclasse da abstrata e ja recebe o dict, retirados os property e o setter uma vez que a classe é para criar colunas e nao fazer validações das inserções isso vai passar para produtomanager

--- models/produto.py
+++ models/produto.py
@@ -1,50 +1,9 @@
-from models.abstrata import EntidadeBase
-from util.logger_util import get_logger
+from sqlalchemy import Column, Integer, String, Float
+from models.abstrata import BaseModel
 
-class Produto(EntidadeBase):
-    def __init__(self, id, nome, preco):
-        super().__init__(id)  # ID já vem da EntidadeBase
-        self._nome = nome
-        self._preco = preco
-        self.logger = get_logger(self.__class__.__name__)
+class Produto(BaseModel):  
+    __tablename__ = "produtos"
 
-    @property
-    def nome(self):
-        return self._nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        if not novo_nome.strip():
-            raise ValueError("O nome do produto não pode estar vazio!")
-        self._nome = novo_nome
-
-    @property
-    def preco(self):
-        return self._preco
-    
-    @preco.setter
-    def preco(self, novo_preco):
-        if novo_preco > 0:
-            self._preco = novo_preco
-            self.logger.info(f"Preço alterado: {self._preco}")
-        else:
-            self.logger.warning("Tentativa de definir preço negativo!")
-            raise ValueError("O preço deve ser positivo.")
-        
-    def to_dict(self):
-        """transforma em um dicionario"""
-        try:
-            self.logger.info(f"Produto convertido para dicionario, {self.nome} | {self.preco}")
-            return {
-                "id": self.id, 
-                "nome":self.nome, 
-                "preco":self.preco
-                }
-            
-        except Exception as e:
-            self.logger.error(f"Erro ao tentar converter para um dicionario, erro: {e}")
-    
-    @classmethod
-    def from_dict(cls, data):
-        """retorna um dicionario"""
-        return cls(data["id"], data["nome"], data["preco"])
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    preco = Column(Float, nullable=False)
