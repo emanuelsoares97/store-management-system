96c3774b65e696996f37dff53ae0db6bb8a61eb4
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-16 13:21:51 +0000

    Update abstrata.py

--- models/abstrata.py
+++ models/abstrata.py
@@ -1,21 +1,39 @@
 from sqlalchemy import Column, Integer
+from sqlalchemy.inspection import inspect
+from util.logger_util import get_logger
+
+logger = get_logger("BaseModel")
 
 class BaseModel:
     """Classe base abstrata para os modelos"""
     __abstract__ = True
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    logger.info("Classe Abstrata iniciada")
 
     @classmethod
     def get_base(cls):
         """Importa Database dentro do método para evitar import circular"""
-        from database import Database
-        return Database().Base
-
+        try:
+            from database import Database
+            logger.debug("Database chamado.")
+            return Database().Base
+        except Exception as e:
+            logger.error(f"Tentativa de importar Database, erro: {e}")
+            raise ValueError(f"Tentativa de importar Database, erro: {str(e)}", exc_info=True)
 
     def to_dict(self):
         """Converte um objeto SQLAlchemy para dicionário JSON"""
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        try:
+            # Garante que a tabela foi inicializada corretamente
+            if not hasattr(self, "__table__"):
+                raise AttributeError("Modelo sem `__table__`, pode estar mal definido.")
+
+            logger.info("Objeto convertido para JSON.")
+            return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+        except Exception as e:
+            logger.error(f"Erro ao converter objeto para JSON: {str(e)}", exc_info=True)
+            return {"erro": "Falha na conversão para JSON"}
 
     @classmethod
     def from_dict(cls, data):
