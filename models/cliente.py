72bfcbb97ff0e9b5165ec9a713a6c1cd58b3c8a0
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-16 13:22:03 +0000

    Update cliente.py

--- models/cliente.py
+++ models/cliente.py
@@ -6,3 +6,4 @@ class Cliente(BaseModel.get_base()):
 
     id = Column(Integer, primary_key=True, autoincrement=True)  #Garantir que `id` fica primeiro!
     nome = Column(String(100), nullable=False)
+    email = Column(String, nullable=False)
