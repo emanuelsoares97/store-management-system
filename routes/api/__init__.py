3069764a86d2f893dae9fb16a4097f726c10e2fd
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-17 09:55:07 +0000

    bp atualizado com as novas rotas

--- routes/api/__init__.py
+++ routes/api/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from routes.api import produtos, login, utilizador  # Importa os módulos das rotas
+from routes.api import produtos, login, utilizador, clientes, vendas, categorias 
 
 def init_routes(app: Flask):
     """Registra todos os Blueprints da aplicação"""
     blueprints = [
         (produtos.produto_bp, "/api/produto"),
         (login.login_bp, "/api/login"),
-        (utilizador.utilizador_bp, "/api/utilizador")
-    ]  # Lista de Blueprints com seus prefixos individuais
-
+        (utilizador.utilizador_bp, "/api/utilizador"),
+        (clientes.cliente_bp, "/api/cliente"),
+        (vendas.venda_bp, "/api/venda"),
+        (categorias.categoria_bp, "/api/categoria")
+    ]
     for bp, prefix in blueprints:
         app.register_blueprint(bp, url_prefix=prefix)  # Registra cada um dinamicamente
