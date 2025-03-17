98c069b43c35bc006615540ba8d9afa1910e2d29
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-17 09:54:29 +0000

    adicionadas colunas a class cliente

--- models/cliente.py
+++ models/cliente.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
 from models.abstrata import BaseModel
 
 class Cliente(BaseModel.get_base()):  
     __tablename__ = "clientes"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)  #Garantir que `id` fica primeiro!
+    id = Column(Integer, primary_key=True, autoincrement=True)  
     nome = Column(String(100), nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, nullable=True)  # Opcional
+    telemovel = Column(String(20), nullable=True)  # Opcional
+    data_registo = Column(DateTime, default=datetime.utcnow)  # Data de criação automática
+    ativo = Column(Boolean, default=True)  # Se está ativo ou não
