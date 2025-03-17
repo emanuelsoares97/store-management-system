979d160176bbb8194913add684919394f8a53540
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-17 12:03:28 +0000

    Update __init__.py

--- routes/api/__init__.py
+++ routes/api/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from routes.api import produtos, login, utilizador, clientes, vendas, categorias 
+from routes.api import auth, produtos, utilizador, clientes, vendas, categorias 
 
 def init_routes(app: Flask):
     """Registra todos os Blueprints da aplicação"""
     blueprints = [
         (produtos.produto_bp, "/api/produto"),
-        (login.login_bp, "/api/login"),
+        (auth.auth_bp, "/api/auth"),
         (utilizador.utilizador_bp, "/api/utilizador"),
         (clientes.cliente_bp, "/api/cliente"),
         (vendas.venda_bp, "/api/venda"),
