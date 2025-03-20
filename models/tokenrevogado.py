5826c000e6e259b95ddf203bce806c08780b8a4b
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-20 20:35:41 +0000

    Update tokenrevogado.py

--- models/tokenrevogado.py
+++ models/tokenrevogado.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String, DateTime
-from datetime import datetime
+from datetime import datetime, timezone
 from models.abstrata import BaseModel
 
-class TokenRevogado(BaseModel.get_base()):
+class TokenRevogado(BaseModel):
     __tablename__ = "tokens_revogados"
 
     id = Column(Integer, primary_key=True)
     token_jti = Column(String, nullable=False, unique=True)  # ID único do token
-    criado_em = Column(DateTime, default=datetime.utcnow)
+    criado_em = Column(DateTime,  default=lambda: datetime.now(timezone.utc))
