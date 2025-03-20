74ca6c178a0870f6b615db400e09a50a0ac0a744
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-20 20:35:34 +0000

    Update categoria.py

--- models/categoria.py
+++ models/categoria.py
@@ -1,9 +1,11 @@
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.abstrata import BaseModel
+from util.logger_util import get_logger
 
-class Categoria(BaseModel.get_base()):
+logger=get_logger(__name__)
+
+class Categoria(BaseModel):
     __tablename__ = "categorias"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
