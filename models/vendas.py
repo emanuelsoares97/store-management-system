d23152da3a7651dc7d37212e73256095076741b7
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-20 20:35:48 +0000

    Update vendas.py

--- models/vendas.py
+++ models/vendas.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer, ForeignKey, DateTime, DECIMAL
 from sqlalchemy.orm import relationship
-from datetime import datetime
+from datetime import datetime, timezone
 from models.abstrata import BaseModel
+from datetime import datetime
 
-class Venda(BaseModel.get_base()):  
+class Venda(BaseModel):  
     __tablename__ = "vendas"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -12,7 +13,7 @@ class Venda(BaseModel.get_base()):
     produto_id = Column(Integer, ForeignKey("produtos.id"), nullable=False)
     quantidade = Column(Integer, nullable=False, default=1)
     valor_total = Column(DECIMAL(10, 2), nullable=False)
-    data_venda = Column(DateTime, default=datetime.utcnow)
+    data_venda = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
 
     # Relacionamentos 
     cliente = relationship("Cliente", backref="compras")
