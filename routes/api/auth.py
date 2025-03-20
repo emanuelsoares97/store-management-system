67460ca9544746cb7efb5e35e4939c7529c976bb
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-20 20:36:42 +0000

    Update auth.py

--- routes/api/auth.py
+++ routes/api/auth.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, request, jsonify, make_response, g
 from services.utilizadoresmanager import UtilizadorService
-from util.auth import AuthService 
+from services.authmanager import AuthService 
 from util.logger_util import get_logger
 from services.tokenrevogadomanager import TokenService
+import re
+import jwt
+from config import Config
+from collections import namedtuple
 
-logger = get_logger("route_auth")
+logger = get_logger(__name__)
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -12,17 +16,38 @@ auth_bp = Blueprint("auth", __name__)
 def login():
     """Autenticação de utilizador"""
     try:
+
+        logger.debug("Rota /api/auth/login chamada.")
+
+        if not request.is_json:
+            logger.info("Formato de envio incorreto, não foi JSON")
+            return jsonify({"erro": "Formato inválido, envie um JSON"}), 400
+        
         data = request.get_json()
+
+        logger.info(f"Dados recebidos: {data}")
 
         if not data or "email" not in data or "password" not in data:
             logger.warning("Tentativa de login sem credenciais fornecidas.")
             return jsonify({"erro": "Email e senha são obrigatórios!"}), 400
 
-        utilizador = UtilizadorService.autenticar(data.get("email"), data.get("password"))
 
+        email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        if not re.match(email_regex, data["email"]):
+            logger.warning("Tenativa de login de email envalido.")
+            return jsonify({"erro": "Email inválido"}), 400
+        
+        
+        
+        utilizador = UtilizadorService.autenticar(data.get("email"), data.get("password")) 
+        
         if not utilizador:
             logger.warning(f"Falha na autenticação para o email: {data.get('email')}")
             return jsonify({"erro": "Credenciais inválidas"}), 403
+        
+        if not utilizador.ativo:  # Garante que a conta está ativa
+            logger.warning(f"Tentativa de login de utilizador desativo: {utilizador.email}")
+            return jsonify({"erro": "Conta desativada. Entre em contato com o suporte."}), 403 
 
         access_token, refresh_token = AuthService.gerar_tokens(utilizador)
 
@@ -59,14 +84,41 @@ def auth():
 @AuthService.token_required  # Já valida o token automaticamente
 def logout():
     """Revoga o access token do utilizador"""
-    token = request.headers.get("Authorization").replace("Bearer ", "")
+    try:
+        token = request.headers.get("Authorization").replace("Bearer ", "")
 
-    # O token já foi validado pelo @AuthService.token_required, então podemos pegar o JTI
-    TokenService.adicionar_token_na_blacklist(g.current_user["jti"])
+        # O token já foi validado pelo @AuthService.token_required, então podemos pegar o JTI
+        TokenService.adicionar_token_na_blacklist(g.current_user["jti"])
+        
+        logger.info("Logout com sucesso")
+        return jsonify({"mensagem": "Logout bem-sucedido!"}), 200
+    
+    except Exception as e:
+        logger.error(f"Erro ao validar token: {str(e)}", exc_info=True)
+        return jsonify({"mensagem": "Token em falta para logout"}), 401
 
-    return jsonify({"mensagem": "Logout bem-sucedido!"}), 200
 
+@auth_bp.route("/refresh", methods=["POST"])
+def refresh_token():
+    """Renova o access token usando o refresh token"""
+    token = request.headers.get("Authorization")  # Pega o token enviado pelo cliente
 
+    if not token:
+        logger.warning
+        return jsonify({"erro": "Refresh token é obrigatório!"}), 401
 
+    token = token.replace("Bearer ", "")  # Remove "Bearer " se estiver presente
 
+    try:
+        payload = jwt.decode(token, Config.SECRET_KEY, algorithms=["HS256"])
+        
+        Utilizador = namedtuple("Utilizador", ["id", "email", "role"])
+        utilizador_obj = Utilizador(id=payload["id"], email="", role="")
 
+        novo_access_token, _ = AuthService.gerar_tokens(utilizador_obj)
+
+        return jsonify({"access_token": novo_access_token}), 200
+    except jwt.ExpiredSignatureError:
+        return jsonify({"erro": "Refresh token expirado. Faça login novamente!"}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({"erro": "Refresh token inválido!"}), 401
