14357cdbadda293461036a63a8e617ca705ecbdd
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-08 19:32:16 +0100

    Update RevokedTokenService.py

--- app/services/RevokedTokenService.py
+++ app/services/RevokedTokenService.py
@@ -1,5 +1,5 @@
 from app.database import Database
-from app.models.revoked_token import TokenRevogado
+from app.models.Revoked_Token import RevokedToken
 from app.util.logger_util import get_logger
 
 logger= get_logger(__name__)
@@ -7,20 +7,18 @@ logger= get_logger(__name__)
 class TokenService:
     """Gerencia a blacklist de tokens JWT"""
 
-    
-
     @staticmethod
-    def adicionar_token_na_blacklist(token_jti):
+    def add_token_to_blacklist(token_jti):
         """Adiciona um token revogado ao banco de dados"""
-        token_revogado = TokenRevogado(token_jti=token_jti)
-        logger.info(f"Token revogado: {token_revogado}")
+        token_revoked= RevokedToken(token_jti=token_jti)
+        logger.info(f"Token revogado: {token_revoked}")
 
         session = Database.get_session()
-        session.add(token_revogado)
+        session.add(token_revoked)
         session.commit()
 
     @staticmethod
-    def esta_na_blacklist(token_jti):
+    def token_on_blacklist(token_jti):
         """Verifica se um token está na blacklist"""
         session = Database.get_session()
-        return session.query(TokenRevogado).filter_by(token_jti=token_jti).first() is not None
+        return session.query(RevokedToken).filter_by(token_jti=token_jti).first() is not None
