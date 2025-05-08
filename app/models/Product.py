eef3b0dfd9c5b7bdaf2c76052ca491c17e7ad61e
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-08 09:12:04 +0100

    alteração dos nomes para ingles

---
+++ app/models/Product.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+from app.models.basemodel import BaseModel
+
+class Product(BaseModel):  
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False, unique=True, index=True)
+    description = Column(Text, nullable=True)
+    price = Column(Float, nullable=False)
+    stock_quantity = Column(Integer, nullable=False)
+    active = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+    category = relationship("Category", back_populates="products")
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
