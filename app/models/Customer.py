47aa9c9dc5c82c32e9ff3435207257d9d18d548b
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-08 09:12:01 +0100

    alteração dos nomes para ingles

---
+++ app/models/Customer.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime, timezone
+from app.models.basemodel import BaseModel
+
+class Customer(BaseModel):  
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String, nullable=True)
+    phone = Column(String(20), nullable=True)
+    registered_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    active = Column(Boolean, default=True)
