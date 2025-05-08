dc01beea91d2d559ec50cd4df47e30748ea34ee9
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-08 09:12:38 +0100

    alteração dos nomes para ingles

---
+++ app/models/Revoked_Token.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
+from app.models.basemodel import BaseModel
+
+class RevokedToken(BaseModel):
+    __tablename__ = "revoked_tokens"
+
+    id = Column(Integer, primary_key=True)
+    token_jti = Column(String, nullable=False, unique=True)  # ID único do token
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
