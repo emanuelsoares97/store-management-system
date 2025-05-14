4a40e210af164d3e3af686d9e177c31742e38a4c
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-14 12:53:55 +0100

    correção do nomes para ingles..

--- app/models/Customer.py
+++ app/models/Customer.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from datetime import datetime, timezone
-from app.models.basemodel import BaseModel
+from app.models.BaseModel import BaseModel
 
 class Customer(BaseModel):  
     __tablename__ = "customers"
