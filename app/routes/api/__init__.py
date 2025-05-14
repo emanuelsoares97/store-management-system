2299521386448df0b02786b6485aeb1db528b8b6
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-14 12:54:07 +0100

    correção do nomes para ingles..

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from app.routes.api import auth, category, customer, product, sale, user 
+from app.routes.api import auth, category, customer, product, sale, user, healthcheck
 
 def init_routes(app: Flask):
     """Registra todos os Blueprints da aplicação"""
+
+    # regista os blueprints
     blueprints = [
-        (product.product_bp, "/api/product"),
-        (auth.auth_bp, "/api/auth"),
-        (user.user_bp, "/api/user"),
-        (customer.customer_bp, "/api/customer"),
-        (sale.sale_bp, "/api/sale"),
-        (category.category_bp, "/api/category")
+        (healthcheck.health_bp, ""),
+        (product.product_bp,  "/api/product"),
+        (auth.auth_bp,        "/api/auth"),
+        (user.user_bp,        "/api/user"),
+        (customer.customer_bp,"/api/customer"),
+        (sale.sale_bp,        "/api/sale"),
+        (category.category_bp,"/api/category"),
     ]
     for bp, prefix in blueprints:
-        app.register_blueprint(bp, url_prefix=prefix)  # Registra cada um dinamicamente
+        app.register_blueprint(bp, url_prefix=prefix)
+
