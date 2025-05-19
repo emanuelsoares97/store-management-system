ca1e6ac2621eb8a106b7d880e361873d75dd6a41
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-19 12:48:40 +0100

    Update healthcheck.py

--- app/routes/api/healthcheck.py
+++ app/routes/api/healthcheck.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 
+#criado para usar como health check no uptimer de modo que a api nao va a baixo
+
 health_bp = Blueprint("health", __name__)
 
 @health_bp.route("/", methods=["GET"])
